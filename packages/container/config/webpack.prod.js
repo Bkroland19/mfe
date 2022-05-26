d538d4f771307df7ca3ae8941b8d9b37ec4cc20a
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2022-05-26 10:02:40 +0300

    configured the public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -1,18 +1,19 @@
-const { merge } = require('webpack-merge');
-const ModuleFederationPlugin = require('webpack/lib/container/ModuleFederationPlugin');
-const commonConfig = require('./webpack.common');
-const packageJson = require('../package.json');
+const { merge } = require("webpack-merge");
+const ModuleFederationPlugin = require("webpack/lib/container/ModuleFederationPlugin");
+const commonConfig = require("./webpack.common");
+const packageJson = require("../package.json");
 
 const domain = process.env.PRODUCTION_DOMAIN;
 
 const prodConfig = {
-  mode: 'production',
+  mode: "production",
   output: {
-    filename: '[name].[contenthash].js',
+    filename: "[name].[contenthash].js",
+    publicPath: "/container/latest/",
   },
   plugins: [
     new ModuleFederationPlugin({
-      name: 'container',
+      name: "container",
       remotes: {
         marketing: `marketing@${domain}/marketing/remoteEntry.js`,
       },
