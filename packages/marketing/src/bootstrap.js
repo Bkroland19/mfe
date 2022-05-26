97620d1ef1f2d215f03dc7a42a32e571aec144e7
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2022-05-26 11:45:36 +0300

    configured bootstrap

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,6 +1,6 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
 // Mount function to start up the app
 const mount = (el) => {
@@ -9,8 +9,8 @@ const mount = (el) => {
 
 // If we are in development and in isolation,
 // call mount immediately
-if (process.env.NODE_ENV === 'development') {
-  const devRoot = document.querySelector('#_marketing-dev-root');
+if (process.env.NODE_ENV === "development") {
+  const devRoot = document.querySelector("#_marketing-dev-root");
 
   if (devRoot) {
     mount(devRoot);
