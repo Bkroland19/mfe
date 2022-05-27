45ebf420e1023ab68ca2eae38cad631b3148f4cc
Bkroland19 <rkbukenya@cis.mak.ac.ug>
2022-05-27 10:12:01 +0300

    auto styles

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -1,15 +1,25 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
+import {
+  StylesProvider,
+  createGenerateClassName,
+} from "@material-ui/core/styles";
 import Header from "./components/Header";
 import MarketingApp from "./components/MarketingApp";
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: "con",
+});
 
 export default () => {
   return (
     <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
+      <StylesProvider generateClassName={generateClassName}>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </StylesProvider>
     </BrowserRouter>
   );
 };
